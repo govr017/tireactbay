d65309c31d574a28da3c992b4918761364ff76fa
Gavrilov Yaroslav <69794668+govr017@users.noreply.github.com>
2022-04-26 23:01:16 +0500

    animtests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { animate, motion } from "framer-motion";
+import { useState } from "react";
+import logo from './papermario.png'
+import download from './download.png'
+import movingtire from './movingtire.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <motion.img src={logo} alt="logo" width={200}
+      whileTap={{rotateY:180}}
+      transition={{yoyo: 10}}
+      ></motion.img>
+      <motion.img src={movingtire} 
+      transition={{yoyo: Infinity}}
+      animate={{y: 10}}
+      ></motion.img>
+      <motion.img src={download}
+      initial = {{scale: 0.4}}
+      whileHover={{scale: 0.6, rotateZ: 360}}
+      whileTap = {{scale: 0.5}}>
+      </motion.img>
+      <div>
+      <motion.b>B</motion.b>
+      </div>
     </div>
   );
 }
