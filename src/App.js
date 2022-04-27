85cce928afabda914541c43142e504b53d1569b4
Gavrilov Yaroslav <69794668+govr017@users.noreply.github.com>
2022-04-27 23:22:56 +0500

    tbay design 2 prot

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import { useState } from "react";
 import logo from './papermario.png'
 import download from './download.png'
 import movingtire from './movingtire.png'
+import './App.css';
 function App() {
   return (
     <div>
-      <motion.img src={logo} alt="logo" width={200}
-      whileTap={{rotateY:180}}
-      transition={{yoyo: 10}}
-      ></motion.img>
+      <header>
       <motion.img src={movingtire} 
-      transition={{yoyo: Infinity}}
+      transition={{yoyo: Infinity, duration: 1}}
       animate={{y: 10}}
       ></motion.img>
-      <motion.img src={download}
-      initial = {{scale: 0.4}}
-      whileHover={{scale: 0.6, rotateZ: 360}}
-      whileTap = {{scale: 0.5}}>
-      </motion.img>
-      <div>
-      <motion.b>B</motion.b>
-      </div>
+      <motion.div transition={{yoyo: Infinity, duration: 3.5, delay: 1}} animate={{y: 15}} className={'logotxt1'}>
+        B
+      </motion.div>
+      <motion.div transition={{yoyo: Infinity, duration: 3.5, delay: 0.5}} animate={{y: 15}} className='logotxt2'>
+        A
+      </motion.div>
+      <motion.div transition={{yoyo: Infinity, duration: 3.5,delay: 1.5}} animate={{y: 15}} className='logotxt3'>
+        Y
+      </motion.div>
+      </header>
+      hey yoeyey
     </div>
   );
 }
